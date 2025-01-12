7cb7a04e37c06aa1020b35f612ec4c9244fe772b
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-01-12 12:46:00 +0100

    Adding sonarr and radarr (Not working)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,12 @@
 {
   description = "Homelab configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
+  outputs = {nixpkgs, ...} @ inputs: {
     nixosConfigurations.homelab = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs pkgs;};
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [./config/configuration.nix];
     };
   };
--- modules/plex.nix
+++ modules/plex.nix
@@ -1,9 +1,33 @@
-{ ... }: {
+{ ... }:
+let
+  config-dir = "/config";
+in {
 
-  services.plex = {
-    enable = true;
-    openFirewall = true;
-    dataDir = "/config/plex"; # Needs changing in future
+  # For sonarr installation
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-wrapped-6.0.36"
+  ];
+
+  services = {
+    plex = {
+      enable = true;
+      openFirewall = true;
+      dataDir = "${config-dir}/plex";
+    };
+
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+      group = "plex";
+      dataDir="${config-dir}/sonarr";
+    };
+    
+    radarr = {
+      enable = true;
+      openFirewall = true;
+      group = "plex";
+      dataDir="${config-dir}/radarr";
+    };
   };
 
 }
