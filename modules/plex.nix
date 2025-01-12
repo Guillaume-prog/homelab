0cdf3e8730b8a15d9c3f3fb1cb4bf2f4f3b94c87
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-01-12 15:28:34 +0100

    Finished adding sonarr and radarr

--- modules/plex.nix
+++ modules/plex.nix
@@ -5,7 +5,9 @@ in {
 
   # For sonarr installation
   nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
     "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
   ];
 
   services = {
