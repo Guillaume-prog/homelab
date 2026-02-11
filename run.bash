36d98c3fb69202e4c37f9d6b3e5877e254729d71
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2026-02-11 07:48:28 +0100

    New multihost configuration

--- run.bash
+++ run.bash
@@ -1,1 +1,2 @@
-ansible-playbook -i src/inventories/hosts.ini src/$1.playbook.yml
+# ansible-playbook -i src/inventories/hosts.ini src/$1.playbook.yml
+ansible-playbook -i inventory/hosts.yml deploy.yml
